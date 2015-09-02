bb4925842777d0375043610e63d4eb5494214c82
cchschmch <christophe.chastanet@gmail.com>
2015-09-02 22:18:23 +0200

    add motor drive

--- Sensor_Led.py
+++ Sensor_Led.py
@@ -9,7 +9,7 @@ class Sensor_Led:
         self._led_right = False
         self.left = 1
         self.right = 0
-        print "Sensor_Led :key K L \n"
+        print "Sensor_Led :key 8 9  \n"
     def on_init(self):
         self.on_cleanup()
         
@@ -20,7 +20,18 @@ class Sensor_Led:
         else:
             led_off(lr)
         return blr
+
             
+    def on_start_led_left(self):
+        self._led_left = self.on_start_stop_led(False,self.left)
+
+    def on_start_led_right(self):
+        self._led_right = self.on_start_stop_led(False,self.right)
+
+    def on_stop_leds(self):
+        self._led_right = self.on_start_stop_led(True,self.right)
+        self._led_left = self.on_start_stop_led(True,self.left)
+
     def on_start_stop_led_left(self):
         self._led_left = self.on_start_stop_led(self._led_left,self.left)
             
@@ -34,9 +45,9 @@ class Sensor_Led:
 
     def on_event(self,event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_k:
+            if event.key == pygame.K_8:
                 self.on_start_stop_led_left()
-            if event.key == pygame.K_l:
+            if event.key == pygame.K_9:
                 self.on_start_stop_led_right()
             
             
